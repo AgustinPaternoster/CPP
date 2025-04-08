e17ed3749e78c185361ea276d3a25abe7fda7986
agustin paternoster <agustinpaternoster@gmail.com>
2025-04-08 16:30:25 +0200

    fin cpp1 ex06

---
+++ CPP_1/ex06/Harl.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	std::cout << "Harl is active" << std::endl;
+}
+
+Harl::~Harl(void)
+{
+	std::cout << "Harl is off" << std::endl;
+}
+
+void Harl::debug(void)
+{
+	std::cout << "[DEBUG]" << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "[INFO]" << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "[WARNING]" << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "[ERROR]" << std::endl;
+}
+
+void Harl::printmsg(void)
+{
+	std::cout << "allowed levels:" << std::endl << "debug"
+			  << std::endl << "info" <<	std::endl << "warning"
+			  << std::endl << "error" << std::endl;
+}
+
+
+void Harl::complain(std::string level)
+{
+	int lvl = -1;
+	typedef void (Harl::*fnpointer)(void);
+	std::string levels[4] = {"debug", "info", "warning", "error"};
+	fnpointer fnp[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0 ; i < 4 ; i++)
+	{
+		if(!levels[i].compare(level))
+			lvl = i;
+	}
+	
+	if (lvl >= 0 && lvl <=3)
+	{
+		for(int i = lvl; i < 4; i++)
+			(this->*fnp[i])();
+		return ;
+	}
+	printmsg();
+
+	
+}
---
+++ CPP_1/ex06/main.cpp
@@ -0,0 +1,10 @@
+#include "Harl.hpp"
+
+int main (int arc, char **argv)
+{
+	(void)arc;
+	Harl harl;
+	
+	harl.complain(argv[1]);
+	return (0);
+}
