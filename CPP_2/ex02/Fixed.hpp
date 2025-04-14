61471641bf284cdd0c3decde3258375f707274e2
agustin paternoster <agustinpaternoster@gmail.com>
2025-04-14 12:27:02 +0200

    inicio cpp2 ex02

---
+++ CPP_2/ex02/Fixed.hpp
@@ -0,0 +1,31 @@
+
+#ifndef __FIXED_H__
+#define __FIXED_H__
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const int i_nb);
+		Fixed(const float f_nb);
+		~Fixed(void);
+		Fixed(const Fixed& scr);
+		
+
+		Fixed& 	operator=(const Fixed& scr);
+		int		getRawBits(void)const;
+		void	setRawBits(int const raw);
+		float	toFloat(void)const;
+		int		toInt(void)const;
+
+	private:
+		static const int	_fractionalBits = 8;
+		int					_fixedPoint;
+
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & rsc);
+#endif
