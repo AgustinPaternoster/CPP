da7dc96f80531cbf653bb82de43f1c70c8089ebd
agustin paternoster <agustinpaternoster@gmail.com>
2025-04-21 16:55:22 +0200

    cpp3 ex00 finished

---
+++ CPP_3/ex00/ClapTrap.hpp
@@ -0,0 +1,29 @@
+#ifndef __CLAPTRAP_HPP__
+#define __CLAPTRAP_HPP__
+
+#include <string>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& rhs);
+		~ClapTrap(void);
+
+		ClapTrap& operator=(const ClapTrap& rhs);
+
+		std::string getName(void)const;
+		void AttributesValues(void);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+
+
+
+	private:
+		std::string 	_name;
+		unsigned int	_hitPoints;
+		unsigned int 	_energyPoints;
+		unsigned int	_attackDamage;
+};
+#endif
