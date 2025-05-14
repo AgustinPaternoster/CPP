e65375da32bb3de31e3642c36291cfdd8b90d76b
AgustinPaternoster <agustinpaternoster@gmail.com>
2025-05-14 13:47:46 +0200

    inicio CPP4 ex03

---
+++ CPP_4/ex03/AMateria.hpp
@@ -0,0 +1,18 @@
+#ifndef __AMATERIA_H__
+#define __AMATERIA_H__
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+    protected:
+        // [...]
+    public:
+        AMateria(std::string const & type);
+        // [...]
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
