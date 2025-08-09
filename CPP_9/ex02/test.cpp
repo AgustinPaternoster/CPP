3c2585bc6098c8e4bdf8f35a938d263bb50f07d8
agustin paternoster <agustinpaternoster@gmail.com>
2025-08-09 09:04:19 +0200

    CPP_9 ex02 binary searh done

--- CPP_9/ex02/test.cpp
+++ CPP_9/ex02/test.cpp
@@ -1,36 +1,25 @@
 #include <vector>
 #include <iostream>
 
-void printVect(std::vector<int> vec, char c)
-{
-	for(std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
-	{
-		std::cout << c << "-" << *it << std:: endl;
-	}
-}
-
-void recursiveOrder(std::vector<int> test)
-{
-	std::vector<int> firstHalf;
-	// std::vector<int> secondHalf;
-	if (test.size() < 2)
-	{
-		printVect(test, '-');
-		return;
-	}
-	for(int i = 0; i < (test.size() / 2) ; i++)
-		firstHalf.push_back(test[i]);
-	recursiveOrder(firstHalf);	
-	// for (int i = test.size() / 2; i < test.size(); i++)
-	// 	secondHalf.push_back(test[i]);
-	printVect(test, 'f');
-	// printVect(secondHalf, 's');
-
-}
-
 int main(int arc, char** argv)
 {
-	std::vector<int> test = {22,4,77, 1 , 123, 6 , 5, 153, 999};
-	recursiveOrder(test);
+	///////////////////////////0 -1 - 2 -3  - 4 - 5 - 6 - 7
+	std::vector<int> result = {3, 7, 12, 18, 25, 31, 40 , 56};
+	// std::vector<int> values = {8, 36 , 0};
+	int value = 50;
+	int indMin = 0;
+	int indMax =  result.size() - 1;
+	int middle;
+	while(indMin != indMax)
+	{
+		middle = (indMin + indMax) / 2;
+		if(value > result[middle])
+			indMin = middle + 1;
+		else
+			indMax = middle - 1;
+	}
+	if (value <= result[indMax])
+		result.insert(result.begin() + indMax, value);
+
 	return (0);
 }
